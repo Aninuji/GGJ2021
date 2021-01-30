825fd95fb1cc9b4e54a9ff664cc16da8e706d203
Lorena.B-at-149097294288 <misatzura@hotmail.com>
2021-01-30 15:26:06 -0600

    menu funcional agregado, faltan creditos

---
+++ Assets/Scripts/MainMenu/parpadeo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parpadeo : MonoBehaviour
+{
+    // Pulse light's range between original range
+    // and half of the original one
+
+    float duration  = 1.5f;
+    float originalRange;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        originalRange = lt.range;
+    }
+
+    void Update()
+    {
+        var amplitude = Mathf.PingPong(Time.time, duration);
+
+        // Transform from 0..duration to 0.5..1 range.
+        amplitude = amplitude / duration * 0.7f + 0.5f;
+
+        // Set light range.
+        lt.range = originalRange * amplitude;
+    }
+}
