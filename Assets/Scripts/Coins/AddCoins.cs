6b85d2de398dbeeb5ff5386c43195f5bc142368b
Lorena.B-at-149097294288 <misatzura@hotmail.com>
2021-01-30 21:18:43 -0600

    globalcoins

---
+++ Assets/Scripts/Coins/AddCoins.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCoins : MonoBehaviour
+{
+    TotalCoins totalCoinsObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCoinsObj = GameObject.FindGameObjectWithTag("GlobalCoins").GetComponent<TotalCoins>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f,30f,0f) * Time.deltaTime);
+        
+    }
+
+     private void OnCollisionEnter(Collision obj){
+        if(obj.gameObject.tag == "Player"){
+            Debug.Log("entr");
+            if(totalCoinsObj != null ){
+                totalCoinsObj.addTotalCoins(1);
+            }
+            Destroy(gameObject);
+        }
+    } 
+}
---
+++ Assets/Scripts/Coins/TotalCoins.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalCoins : MonoBehaviour
+{
+
+   // private int minCoins = 0;
+    private int totalCoins;
+    
+    [TextArea]
+    public string Notes = "Comment Here.";
+    public TextMesh coinsMesh; 
+
+    void Awake(){
+        DontDestroyOnLoad(gameObject);
+       
+    }
+
+    public void addTotalCoins(int coins){
+        totalCoins = totalCoins + coins;
+        if(coinsMesh != null){
+            setText();
+        }
+    }
+
+    public void diffTotalCoins(int coins){
+        totalCoins = totalCoins - coins;
+        if(coinsMesh != null){
+            setText();
+        }
+    }
+
+    private void setText(){
+        coinsMesh.text = "Coins: " + totalCoins;
+    }
+}
