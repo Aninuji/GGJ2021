48e2eef24f653a26c9cc3ad971b25738e28af63e
Lorena.B-at-149097294288 <misatzura@hotmail.com>
2021-01-30 21:48:54 -0600

    new changes to totalcoins script

--- Assets/Scripts/Managers/TotalCoins.cs
+++ Assets/Scripts/Managers/TotalCoins.cs
@@ -9,12 +9,6 @@ public class TotalCoins : Singleton<TotalCoins>
    // private int minCoins = 0;
     private int totalCoins;
     public Text coinsText; 
-    public int totalCoins;
-    
-    [TextArea]
-    public string Notes = "Comment Here.";
-    public TextMesh coinsMesh; 
-
     void Awake(){
         DontDestroyOnLoad(gameObject);
        
