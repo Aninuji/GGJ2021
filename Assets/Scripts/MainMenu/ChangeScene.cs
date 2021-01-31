231b0d2bf70eb4a571ff7867bc70262ba03576ce
Lorena.B-at-149097294288 <misatzura@hotmail.com>
2021-01-30 22:58:41 -0600

    menu sounds

--- Assets/Scripts/MainMenu/ChangeScene.cs
+++ Assets/Scripts/MainMenu/ChangeScene.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
 
 public class ChangeScene : MonoBehaviour
 {
+
     public void changeScene(string sceneNameTarget){
+        //IEnumerator coroutine = Waiter(sceneNameTarget);
+        Debug.Log("changescente");
+        StartCoroutine(Waiter(sceneNameTarget));
+       
+    }
+
+    private IEnumerator Waiter(string sceneNameTarget)
+    {
+        Debug.Log("waiter");
+       yield return new WaitForSeconds(0.5f);
+       Debug.Log("after wait");
         SceneManager.LoadScene(sceneNameTarget);
     }
 }
